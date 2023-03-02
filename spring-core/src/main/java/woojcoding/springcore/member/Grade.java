5083d70f67da4bf7920a921e338ee3125d48d1e5
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-03-02 15:54:56 +0900

    회원 도메인 설계
    회원가입, 조회가 가능하도록 설계함.
    현재 MemberService 구현체에서 MemberRepository 인터페이스와 MemoryMemberRepository 구현체를 모두 의존하고 있어 DIP 원칙을 잘 지키고 있지 않다.
    따라서 다른 저장소로 변경할 때 OCP 원칙 또한 준수할 수 없는 상황이다.

---
+++ spring-core/src/main/java/woojcoding/springcore/member/Grade.java
@@ -0,0 +1,6 @@
+package woojcoding.springcore.member;
+
+public enum Grade {
+    BASIC,
+    VIP
+}
---
+++ spring-core/src/main/java/woojcoding/springcore/member/MemberApp.java
@@ -0,0 +1,16 @@
+package woojcoding.springcore.member;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberServiceImpl memberService = new MemberServiceImpl();
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+
+        System.out.println("member = " + member.getName());
+        System.out.println("findMember = " + findMember.getName());
+    }
+}
