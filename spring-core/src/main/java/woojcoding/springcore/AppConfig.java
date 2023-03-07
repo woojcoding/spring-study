fed302e96f0ac549468e35f70402a6c0d588b1e2
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-03-07 11:02:14 +0900

    AppConfig 리팩토링
    객체를 생성하는 중복 코드를 제거하였음.
    역할과 구현 클래스가 한눈에 들어오도록 개선하였음.
    애플리케이션 전체 구성을 더욱 빠르게 파악할 수 있을 것으로 기대됨.

--- spring-core/src/main/java/woojcoding/springcore/AppConfig.java
+++ spring-core/src/main/java/woojcoding/springcore/AppConfig.java
@@ -1,6 +1,8 @@
 package woojcoding.springcore;
 
+import woojcoding.springcore.discount.DiscountPolicy;
 import woojcoding.springcore.discount.FixDiscountPolicy;
+import woojcoding.springcore.member.MemberRepository;
 import woojcoding.springcore.member.MemberService;
 import woojcoding.springcore.member.MemberServiceImpl;
 import woojcoding.springcore.member.MemoryMemberRepository;
@@ -9,12 +11,20 @@ import woojcoding.springcore.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
         return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+                memberRepository(),
+                discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
