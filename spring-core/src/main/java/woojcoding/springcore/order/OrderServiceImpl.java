3b09fcc64a3af3a5052e322ffb3f5a1176425268
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-03-28 17:58:16 +0900

    롬복
    롬복 라이브러리 적용 - 롬복이 자바의 애너테이션 프로세서 기능을 이용하여 컴파일 시점에 코드를 자동으로 생성해준다. @RequiredArgsConstructor를 사용하여 final 필드의 생성자를 자동으로 생성하였음.

--- spring-core/src/main/java/woojcoding/springcore/order/OrderServiceImpl.java
+++ spring-core/src/main/java/woojcoding/springcore/order/OrderServiceImpl.java
@@ -1,24 +1,18 @@
 package woojcoding.springcore.order;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import woojcoding.springcore.discount.DiscountPolicy;
 import woojcoding.springcore.member.Member;
 import woojcoding.springcore.member.MemberRepository;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
     // 인터페이스에만 의존하도록 코드 변경
     private final MemberRepository memberRepository;
 
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository,
-                            DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
