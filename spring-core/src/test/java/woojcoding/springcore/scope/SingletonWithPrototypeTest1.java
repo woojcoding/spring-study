d97a9a396e523d3bad7db20f6eb9498c0edb3a9b
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-03-29 17:43:53 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용하는 경우 문제 발생
    싱글톤 빈이 생성될 시에 의존관계 주입을 받는다. 프로토타입 빈이 새로 생성되어 주입된다. 그 후 프로토타입 빈의 메서드를 호출하더라도 새로 생성되지 않고, 계속 같은 프로토타입 빈을 사용하게 되는 문제점이 발생한다.

---
+++ spring-core/src/test/java/woojcoding/springcore/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,85 @@
+package woojcoding.springcore.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class SingletonWithPrototypeTest1 {
+    @Test
+    void prototypeFind() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        PrototypeBean prototypeBean1 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+
+        assertThat(prototypeBean1.getCount()).isEqualTo(1);
+
+        PrototypeBean prototypeBean2 = ac.getBean(PrototypeBean.class);
+        prototypeBean1.addCount();
+
+        assertThat(prototypeBean1.getCount()).isEqualTo(2);
+    }
+
+    @Test
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac =
+                new AnnotationConfigApplicationContext(
+                        ClientBean.class, PrototypeBean.class
+                );
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int cnt1 = clientBean1.logic();
+
+        assertThat(cnt1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int cnt2 = clientBean1.logic();
+
+        assertThat(cnt2).isEqualTo(2);
+    }
+
+    static class ClientBean {
+        private final PrototypeBean prototypeBean; // 생성 시점에 이미 주입되어 있음
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic() {
+            prototypeBean.addCount();
+
+            return prototypeBean.getCount();
+        }
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
