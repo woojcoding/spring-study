d0d76bf9ede983b90ccf02671c5982eefc48e919
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-03-30 14:44:06 +0900

    웹 스코프 - 문제 해결
    request 스코프 빈을 생성자 주입해주는 것이 아니라 ObjectProvider를 생성자 주입 후 HTTP 요청이 온 경우에 getObject()를 호출한다면 스프링컨테이너에서 빈을 조회 후 없기에 새로운 빈을 생성하여 반환해준다. 이러한 방법으로 request 스코프 빈을 사용할 수 있다. 이 빈은 같은 HTTP 요청이라면 새롭게 생성되지 않고 같은 빈이 반환된다.

--- spring-core/src/main/java/woojcoding/springcore/web/LogDemoController.java
+++ spring-core/src/main/java/woojcoding/springcore/web/LogDemoController.java
@@ -1,6 +1,7 @@
 package woojcoding.springcore.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,13 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
     private final LogDemoService logDemoService;
 
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
 
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
 
--- spring-core/src/main/java/woojcoding/springcore/web/LogDemoService.java
+++ spring-core/src/main/java/woojcoding/springcore/web/LogDemoService.java
@@ -1,15 +1,17 @@
 package woojcoding.springcore.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import woojcoding.springcore.common.MyLogger;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
 
     public void logic(String testId) {
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("sevice id = " + testId);
     }
 }
