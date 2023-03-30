a09e140004d03c5a5cbba45cffbeb20b7fa9a203
woojcoding <110895459+woojcoding@users.noreply.github.com>
2023-03-30 15:14:38 +0900

    웹 스코프 - 문제 해결
    스코프를 지정시 프록시 모드 설정을 할 수 있다. 프록시 설정을 해준다면 가짜 프록시 객체를 주입해준다. 실제 객체의 로직을 호출하는 순간 프록시 객체가 진짜 객체의 로직을 호출해준다. 해당 애너테이션 설정만으로 싱글톤 빈을 사용하듯 쉽게 사용할 수 있다.

--- spring-core/src/main/java/woojcoding/springcore/web/LogDemoController.java
+++ spring-core/src/main/java/woojcoding/springcore/web/LogDemoController.java
@@ -1,7 +1,6 @@
 package woojcoding.springcore.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,14 +13,16 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
     private final LogDemoService logDemoService;
 
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
 
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
+        //class woojcoding.springcore.common.MyLogger$$EnhancerBySpringCGLIB$$67b765e1 프록시 객체
+        System.out.println(myLogger.getClass());
+
         myLogger.setRequestURL(requestURL);
         myLogger.log("controller test");
 
--- spring-core/src/main/java/woojcoding/springcore/web/LogDemoService.java
+++ spring-core/src/main/java/woojcoding/springcore/web/LogDemoService.java
@@ -1,17 +1,15 @@
 package woojcoding.springcore.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 import woojcoding.springcore.common.MyLogger;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
-    private final ObjectProvider<MyLogger> myLoggerObjectProvider;
+    private final MyLogger myLogger;
 
     public void logic(String testId) {
-        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.log("sevice id = " + testId);
     }
 }
